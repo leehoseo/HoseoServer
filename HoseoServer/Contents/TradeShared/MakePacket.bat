54b54c69e121c2f7919d187e6645fde89d0a35de
leehoseo <leehoseo@game.hoseo.edu>
2023-03-08 21:44:18 +0900

    테스트 fbs 파일 수정, .bat을 프로젝트에 추가

--- HoseoServer/Contents/TradeShared/MakePacket.bat
+++ HoseoServer/Contents/TradeShared/MakePacket.bat
@@ -1,6 +1,6 @@
 SET solutionPath=%1
 SET projectPath=%2
 
-start "" "%solutionPath%trd\FlatBuffer\GenerateFlatBuffer.bat" %solutionPath% %projectPath% Person.fbs
+start "" "%solutionPath%trd\FlatBuffer\GenerateFlatBuffer.bat" %solutionPath% %projectPath% Trade.fbs
 
 exit
